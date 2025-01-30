6ef6c90c56aed0bff498c01218f7f9dbf74307f4
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-30 14:40:57 +0100

    reverted back app jsx

--- keynest-frontend/src/App.jsx
+++ keynest-frontend/src/App.jsx
@@ -11,27 +11,22 @@ import About from "./pages/About";
 import Apartment from "./pages/Apartment";
 import NotFound from "./pages/NotFound";
 
-/* context */
-import { LanguageProvider } from "./context/LanguageContext";
-
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <div className="app-container">
-          <HeaderComp />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/apartment/:id" element={<Apartment />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <FooterComp />
-        </div>
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <div className="app-container">
+        <HeaderComp />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/apartment/:id" element={<Apartment />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <FooterComp />
+      </div>
+    </Router>
   );
 }
 
