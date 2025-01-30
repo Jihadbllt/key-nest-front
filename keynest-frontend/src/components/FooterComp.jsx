38a31670b1de05aea844bdffb5bd48aeb6f22cd9
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-30 14:39:20 +0100

    reverted everything

--- keynest-frontend/src/components/FooterComp.jsx
+++ keynest-frontend/src/components/FooterComp.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useLanguage } from "../context/LanguageContext";
 import footerLogo from "../assets/key-logo-footer.svg";
 import "./FooterComp.css";
 
 const FooterComp = () => {
-  const { currentLang, switchLanguage } = useLanguage();
   const date = new Date();
   const year = date.getFullYear();
 
@@ -14,28 +12,6 @@ const FooterComp = () => {
       <Link to="/" className="footer-logo">
         <img src={footerLogo} alt="KeyNest logo" />
       </Link>
-      
-      <div className="language-switcher">
-        <button 
-          onClick={() => switchLanguage('en')}
-          className={currentLang === 'en' ? 'active' : ''}
-        >
-          English
-        </button>
-        <button
-          onClick={() => switchLanguage('fr')}
-          className={currentLang === 'fr' ? 'active' : ''}
-        >
-          Français
-        </button>
-        <button
-          onClick={() => switchLanguage('sp')}
-          className={currentLang === 'sp' ? 'active' : ''}
-        >
-          Español
-        </button>
-      </div>
-
       <p>&copy; {year} KeyNest</p>
     </footer>
   );
