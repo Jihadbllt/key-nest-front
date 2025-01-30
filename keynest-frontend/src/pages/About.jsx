2b2fee6e0616c0e1a38a3468ef7537195260775d
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2025-01-30 14:30:13 +0100

    edited about jsx

--- keynest-frontend/src/pages/About.jsx
+++ keynest-frontend/src/pages/About.jsx
@@ -1,28 +1,26 @@
 import React from "react";
+import { useLanguage } from "../context/LanguageContext";
 import BannerComp from "../components/BannerComp";
 import Accordion from "../components/AccordionComp";
 import bannerImage from "../assets/cabin.jpg";
 import "./About.css";
 
 function About() {
+  const { currentLang } = useLanguage();
+  const aboutData = require(`../data/data.about.${currentLang}.json`);
+
   return (
     <div>
-      <h1 className="about-title">KeyNest's Values</h1>
+      <h1 className="about-title">{aboutData.title}</h1>
       <BannerComp imageUrl={bannerImage} />
       <div className="about-content">
-        
-        <Accordion
-          title="Reliability"
-          content="The listings posted on KeyNest guarantee complete reliability. The photos match the accommodations, and all information is regularly verified by our teams."
-        />
-        <Accordion
-          title="Service"
-          content="Our service ensures that your needs are met at every step. From booking to check-out, we’re here for you."
-        />
-        <Accordion
-          title="Security"
-          content="KeyNest places the highest priority on ensuring the safety of all guests and hosts with robust processes and security checks."
-        />
+        {aboutData.accordionItems.map((item, index) => (
+          <Accordion
+            key={index}
+            title={item.title}
+            content={item.content}
+          />
+        ))}
       </div>
     </div>
   );
